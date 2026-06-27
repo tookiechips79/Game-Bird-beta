54ca13d8b23c0a1b21c8dee6c302e7bc42a0b112
randallpaguio <tookiechips@gmail.com>
2026-06-27 03:15:55 -0700

    Add CommonJS socket server for Railway compatibility

---
+++ socket-server.cjs
@@ -0,0 +1,65 @@
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: '*', methods: ['GET', 'POST'] }
+});
+
+let gameState = null;
+let usersState = null;
+let historyState = null;
+let lastStampedClientTs = null;
+
+io.on('connection', (socket) => {
+  console.log(`[socket] client connected: ${socket.id} (${io.engine.clientsCount} total)`);
+
+  if (gameState) socket.emit('game:state', gameState);
+  if (usersState) socket.emit('users:state', usersState);
+  if (historyState) socket.emit('history:state', historyState);
+
+  socket.on('game:update', (newState) => {
+    let state = newState;
+    let serverModified = false;
+
+    if (newState.isTimerRunning && newState.timerStartedAt &&
+        newState.timerStartedAt !== lastStampedClientTs) {
+      lastStampedClientTs = newState.timerStartedAt;
+      state = { ...newState, timerStartedAt: Date.now() };
+      serverModified = true;
+    }
+
+    gameState = state;
+
+    if (serverModified) {
+      io.emit('game:state', state);
+    } else {
+      socket.broadcast.emit('game:state', state);
+    }
+  });
+
+  socket.on('time:ping', (clientTs) => {
+    socket.emit('time:pong', { clientTs, serverTs: Date.now() });
+  });
+
+  socket.on('users:update', (newUsers) => {
+    usersState = newUsers;
+    socket.broadcast.emit('users:state', newUsers);
+  });
+
+  socket.on('history:update', (newHistory) => {
+    historyState = newHistory;
+    socket.broadcast.emit('history:state', newHistory);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`[socket] client disconnected: ${socket.id}`);
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+httpServer.listen(PORT, () => {
+  console.log(`GameBird socket server running on :${PORT}`);
+});
